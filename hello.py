b269a643245a0d00a2e298d6243d360ece662e02
Shayne Meyer <shayne.meyer@gmail.com>
2017-06-13 06:29:57 -0700

    get method login example

--- hello.py
+++ hello.py
@@ -1,23 +1,33 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return url_for('show_user_profile', username='Shayne')
+# @app.route('/')
+# def index():
+#     return url_for('show_user_profile', username='Shayne')
+#
+# @app.route('/hello')
+# def hello_world():
+#     # import pdb; pdb.set_trace()
+#     return 'Hello World!'
+#
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#     # show the user profile for that user
+#     return "User %s" % username
+#
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+#     return "Post %d" % post_id
 
-@app.route('/hello')
-def hello_world():
-    # import pdb; pdb.set_trace()
-    return 'Hello World!'
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return "User %s" % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return "Post %d" % post_id
+@app.route('/login', methods=['GET'])
+def login():
+    if request.values:
+        return 'username is ' + request.values["username"]
+    else:
+        return '<form method="get" action="/login">' \
+           '<input type="text" name="username" />' \
+           '<p></p>' \
+           '<button type="submit">Submit</button></form>'
 
 if __name__ == '__main__':
     app.debug = True
