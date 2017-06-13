b981f70d94fcb27d7252605907f2482a5076b75b
Shayne Meyer <shayne.meyer@gmail.com>
2017-06-13 07:11:31 -0700

    finished login template and methods

--- hello.py
+++ hello.py
@@ -30,10 +30,27 @@ app = Flask(__name__)
 #            '<button type="submit">Submit</button></form>'
 
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+# @app.route('/hello')
+# @app.route('/hello/<name>')
+# def hello(name=None):
+#     return render_template('hello.html', name=name)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'], request.form['password']):
+            return "Welcome back %s" % request.form['username']
+        else:
+            error = 'Incorrect username and password'
+
+    return render_template('login.html', error=error)
+
+def valid_login(username, password):
+    if username == password:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     app.debug = True
